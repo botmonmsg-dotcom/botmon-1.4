421a2c1ab612bbd298439431815834817e51b0cf
botmonmsg-dotcom <botmonmsg@gmail.com>
2025-08-23 19:23:12 +0200

    Updated Simple Page
    Simple Basics

--- page.js
+++ page.js
@@ -9,26 +9,64 @@ function showSection(value) {
         case "subscription": {
             uncollpase("access")
             uncollpase("product")
+
+            break;
         }
         case "access": {
             uncollpase("subscription")
             uncollpase("product")
+            break;
         }
         case "product": {
             uncollpase("subscription")
             uncollpase("access")
+            break;
         }
     }
+    refreshAnchor(value)
+}
+
+function refreshAnchor(value) {
+    let elements = document.getElementsByClassName("anchor-define");
+
+    for (let i = 0; i < elements.length; i++) {
+        let element=elements[i]
+
+        if(element.classList.contains("isActive")){
+            element.classList.remove("isActive")
+            element.setAttribute("href","#get_"+value)
+        }
+        else if(!element.classList.contains("isActive")){
+            element.classList.add("isActive")
+            element.setAttribute("href","#products_info")
+        }
+    }
+
 }
 
 function uncollpase(value) {
     let element = document.getElementById(value)
     if (element.classList.contains("show")) {
-        document.getElementById("get_"+value).click()
+        document.getElementById("get_" + value).click()
     }
+}
+
+function showCanvas(value) {
+    document.getElementById(value).classList.add("show")
+}
+function unshowCanvas(value) {
+    document.getElementById(value).classList.remove("show")
 }
 
 function clickViewBoard() {
     let viewBoard = document.getElementsByClassName("viewBoardCard")[0]
     viewBoard.click()
+
+}
+
+var myModal = new bootstrap.Modal(document.getElementById('simpleModal'), {})
+//myModal.show()
+
+function showModal(value){
+    new bootstrap.Modal(document.getElementById(value), {}).show()
 }
